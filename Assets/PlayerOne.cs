cbd49dca10b1d8805f0a02db8f1aac55bc03a0b1
Nova Caster <casanovawarleyne@gmail.com>
2024-09-24 17:31:09 +0800

    Player One
    Shows player one details when button is pressed.

--- Assets/PlayerOne.cs
+++ Assets/PlayerOne.cs
@@ -5,5 +5,19 @@ using TMPro;
 
 public class PlayerOne : MonoBehaviour
 {
-    int = 5
+    [SerializeField] private TextMeshProUGUI Info;
+
+    string Name = "Kon";
+    int Health = 150;
+    int Mana = 100;
+    int ATK = 20;
+    int MATK = 80;
+    int DEF = 30;
+    int MRES = 50;
+    float SPD = 45.5f;
+
+    public void ButtonPressed()
+    {
+        Info.text = "Details:\r\n \r\nName: " + Name + "\r\nHealth: " + Health + "\r\nMana: " + Mana + "\r\nATK: " + ATK + "\r\nM. ATK: " + MATK + "\r\nDEF: " + DEF + "\r\nM. RES: " + MRES + "\r\n SPD: " + SPD;
+    }
 }
